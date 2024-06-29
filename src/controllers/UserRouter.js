329ef0e9824e400cc95511582869d27e6d8da296
Dunconius <duncanbean84@gmail.com>
2024-06-29 11:11:58 +1000

    fix: fixed user routers

--- src/controllers/UserRouter.js
+++ src/controllers/UserRouter.js
@@ -1,48 +1,47 @@
 const express = require("express");
-const { UserModel } = require("../models/UserModel");
+const { UserModel } = require("../models/UserModel"); // Need this imported somewhere in the server code to make the server connection use Users
+const { comparePasswords, createJwt } = require("../utils/authHelpers");
 const router = express.Router();
 
 router.get("/", async (request, response, next) => {
-    
-    let result = await UserModel.find({}).exec();
-    
-    response.json({
-        message:"User router operation...",
-        result: result
-    });
+
+	let result = await UserModel.find({}).exec();
+
+	response.json({
+		message:"User router operation",
+		result: result
+	});
 });
 
 
 router.get("/findById/:id", async (request, response, next) => {
-    
-    let result = await UserModel.findById(request.params.id).exec();
 
-    response.json({
-        message:"User result by ID...",
-        result: result
-    });
+	let result = await UserModel.findById(request.params.id).exec();
+
+	response.json({
+		message:"User router operation",
+		result: result
+	});
 });
-
 
 router.post("/findOneQuery", async (request, response, next) => {
-    
-    let result = await UserModel.findOne(request.body).exec();
 
-    response.json({
-        message:"User result by ID...",
-        result: result
-    });
+	let result = await UserModel.findOne(request.body).exec();
+
+	response.json({
+		message:"User router operation",
+		result: result
+	});
 });
 
-
 router.post("/findManyQuery", async (request, response, next) => {
-    
-    let result = await UserModel.find(request.body).exec();
 
-    response.json({
-        message:"User result by ID...",
-        result: result
-    });
+	let result = await UserModel.find(request.body).exec();
+
+	response.json({
+		message:"User router operation",
+		result: result
+	});
 });
 
 
@@ -53,16 +52,18 @@ router.post("/", async (request, response, next) => {
 		return error
 	});
 
+	let jwt = createJwt(result._id);
+
 	if (result.errors) {
 		return next(result);
 	}
 
 	response.json({
 		message:"User router operation",
-		result: result
+		result: result,
+		jwt: jwt
 	});
 });
-
 
 router.patch("/findById/:id", async (request, response, next) => {
 
@@ -80,7 +81,6 @@ router.patch("/findById/:id", async (request, response, next) => {
 	});
 });
 
-
 router.delete("/", async (request, response, next) => {
 
 	let result = await UserModel.findByIdAndDelete(request.body.id);
@@ -90,6 +90,42 @@ router.delete("/", async (request, response, next) => {
 		result: result
 	});
 });
+
+
+
+// Login route 
+router.post("/jwt", async (request, response, next) => {
+	let newJwt = "";
+
+	if (!request.body.password || !request.body.username){
+		return next(new Error("Missing login details in login request."));
+	}
+
+	// Find user by username in DB
+	let foundUser = await UserModel.findOne({username: request.body.username}).exec();
+
+	// Compare request.body.password to foundUser.password using the compare function 
+	let isPasswordCorrect = await comparePasswords(request.body.password, foundUser.password);
+
+
+	// Create a JWT based on foundUser._id 
+	if (isPasswordCorrect){
+
+		newJwt = createJwt(foundUser._id);
+
+		response.json({
+			jwt: newJwt
+		});
+	} else {
+		return next(new Error("Incorrect password."));
+	}
+
+	
+})
+
+// Validate JWT route 
+
+
 
 
 module.exports = router;
